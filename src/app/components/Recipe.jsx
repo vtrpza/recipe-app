7b401bc14558dd170625c4772f6e3a27a50f6581
pza <vhnpouza@gmail.com>
2023-07-02 17:54:14 -0300

    first

--- src/app/components/Recipe.jsx
+++ src/app/components/Recipe.jsx
@@ -1,15 +1,33 @@
-"use client";
-import React from "react";
-import { Card, CardContent, Typography } from "@mui/material";
+import React, { useEffect, useRef } from "react";
 
-function Recipe({ recipe }) {
+function Recipe({ recipe, theme }) {
+  const ref = useRef();
+
+  useEffect(() => {
+    const boxShadow = getComputedStyle(ref.current)
+      .getPropertyValue("--primary-glow")
+      .trim();
+    ref.current.style.boxShadow = boxShadow;
+  }, [theme]);
+
   return (
-    <Card>
-      <CardContent>
-        <Typography variant="h5">{recipe.name}</Typography>
-        {/* Add more details about the recipe here */}
-      </CardContent>
-    </Card>
+    <div
+      ref={ref}
+      style={{
+        width: { xs: "100%", sm: "85%", md: "70%" },
+        minHeight: "252px",
+        marginBottom: "2px",
+        borderRadius: "12px",
+      }}
+    >
+      <div>
+        <h5>{recipe.name}</h5>
+        <p>Tempo de preparo: {recipe.preparation_time}</p>
+        <p>Porções: {recipe.portions}</p>
+        <p>Autor: {recipe.author}</p>
+        <p>Likes: {recipe.likes}</p>
+      </div>
+    </div>
   );
 }
 
--- src/app/components/RecipeList.jsx
+++ src/app/components/RecipeList.jsx
@@ -1,9 +1,11 @@
 "use client";
-import React, { useEffect, useState } from "react";
+
+import React, { useEffect, useState, useRef } from "react";
 import Recipe from "./Recipe";
 
 function RecipeList() {
   const [recipes, setRecipes] = useState([]);
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
     fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/recipes`)
@@ -11,11 +13,39 @@ function RecipeList() {
       .then((data) => setRecipes(data));
   }, []);
 
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
-    <div>
-      {recipes.map((recipe) => (
-        <Recipe key={recipe.id} recipe={recipe} />
-      ))}
+    <div
+      style={{
+        maxWidth: "1100px",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill,minmax(300px,1fr))",
+          gap: "2px",
+          justifyContent: "center",
+        }}
+      >
+        {recipes.map((recipe) => (
+          <Recipe key={recipe.id} recipe={recipe} theme={theme} />
+        ))}
+      </div>
+      <button onClick={toggleTheme}>
+        {theme === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}
+      </button>
     </div>
   );
 }
